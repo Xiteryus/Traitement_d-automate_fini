364b1d7f75a10cf1f97db3c3de6a59b174381aa3
samue <xiteryus@gmail.com>
2024-04-17 14:20:33 +0200

    V5

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ def lire_automate_sur_fichier(nom_fichier):
 def afficher_automate(automate):
     print("Alphabet:", automate['alphabet'])
     print("Nombre d'états:", automate['nb_etats'])
-    print("États initiaux:", automate['etats_initiaux'])
-    print("États terminaux:", automate['etats_terminaux'])
+    print("États initiaux:",  ', '.join(['(' + ', '.join(etat) + ')' for etat in automate['etats_initiaux']]))
+    print("États terminaux:",  ', '.join(['(' + ', '.join(etat) + ')' for etat in automate['etats_terminaux']]))
     print("Transitions:")
     for transition in automate['transitions']:
-        print(transition)
+        etat_source, symbole, etat_destination = transition
+        print(f"({', '.join(etat_source)})", symbole, f"({', '.join(etat_destination)})")
 
 #STANDARDISATION :
 
@@ -43,6 +44,7 @@ def est_standard(automate):
 
 
 def standardisation(automate):
+    test_vide = False
     if est_standard(automate) == False :
         nouvel_etat_initial = str(automate['nb_etats'])
         automate['nb_etats'] += 1
@@ -83,10 +85,57 @@ def est_deterministe(automate):
 
 def Determinisation(automate):
 
+    if len(automate['etats_initiaux']) != 1:
+        standardisation(automate)
+
+    etats_visites = set()
+    etats_a_traiter = [tuple(automate['etats_initiaux'])]
+
+    nouveaux_etats = {}
+    nouvelles_transitions = []
+
+    while etats_a_traiter:
+        etat_courant = etats_a_traiter.pop()
+        etats_visites.add(etat_courant)
+
+        transitions_par_symbole = {}
+        for symbole in automate['alphabet']:
+            etats_suivants = set()
+            for etat in etat_courant:
+                for transition in automate['transitions']:
+                    if transition[0] == etat and transition[1] == symbole:
+                        etats_suivants.add(transition[2])
+            transitions_par_symbole[symbole] = etats_suivants
+
+        for symbole, etats_suivants in transitions_par_symbole.items():
+            nouvel_etat = tuple(sorted(list(etats_suivants)))
+            nouvelles_transitions.append((etat_courant, symbole, nouvel_etat))
+
+            if nouvel_etat not in etats_visites:
+                etats_a_traiter.append(nouvel_etat)
+
+        nouveaux_etats[etat_courant] = None
+
+    automate_deterministe = {
+        'alphabet': automate['alphabet'],
+        'nb_etats': len(nouveaux_etats),
+        'etats_initiaux': [tuple(automate['etats_initiaux'])],
+        'etats_terminaux': [],
+        'transitions': nouvelles_transitions
+    }
+
+    for etat, _ in nouveaux_etats.items():
+        for etat_terminal in automate['etats_terminaux']:
+            if etat_terminal in etat:
+                automate_deterministe['etats_terminaux'].append(etat)
 
 
+    print("L'automate est standard :", est_standard(automate_deterministe))
+    print("L'automate est deterministe :", est_deterministe(automate_deterministe))
+    print("L'automate est complet :", est_complet(automate_deterministe))
 
-    return automate
+
+    return automate_deterministe
 
 
 #COMPLETION
@@ -112,7 +161,7 @@ def completion(automate):
         for lettre in automate['alphabet']:
             transition_existante = False
             for transition in automate['transitions']:
-                etat_depart, symbole, etat_arrivee = transition
+                etat_depart, symbole, _ = transition
                 if etat_depart == str(etat) and symbole == lettre:
                     transition_existante = True
                     break
@@ -134,9 +183,12 @@ def automate_complementaire(automate):
     automate['etats_terminaux'] = list(etats - set(automate['etats_terminaux']))
 
     return automate
+
+
+
 def main():
     # Lecture de l'automate depuis un fichier
-    nom_fichier = "Automates.txt"
+    nom_fichier = "Test1.txt"
     automate = lire_automate_sur_fichier(nom_fichier)
     # Affichage de l'automate
     afficher_automate(automate)
@@ -145,13 +197,14 @@ def main():
     print("L'automate est deterministe :", est_deterministe(automate))
     print("L'automate est complet :", est_complet(automate))
 
+    if input("Voulez-vous le determiniser ? (oui/non) : ") == "oui":
+        afficher_automate(Determinisation(automate))
+
     if input("Voulez-vous le complementariser ? (oui/non) : ") == "oui":
         automate_complementaire(automate)
         afficher_automate(automate)
 
-    if input("Voulez-vous le determiniser ? (oui/non) : ") == "oui":
-        Determinisation(automate)
-        afficher_automate(automate)
+
 
     if input("Voulez-vous le Completer ? (oui/non) : ") == "oui":
         completion(automate)
